8858f8c2f43c6b90dac5d29952873701f81a5bd6
Paul Arlott <paul@the7thdream.com>
2013-09-11 11:42:25 +0800

    Updated upload buttons to update the text on selection

--- js/fusionCSS.js
+++ js/fusionCSS.js
@@ -3,5 +3,5 @@
  @link http://fusionCSS.com
 */
 window.fusionLib||(window.$fl=window.fusionLib=jQuery);
-$fl(document).ready(function(){$fl(".collapseMenu").each(function(a){var c='<option value="" selected="selected">Menu...</option>';$fl(this).find("a").each(function(){for(var d=$fl(this),a=0,e="",b=d.parent();null!=b[0];)"ul"==b[0].nodeName.toLowerCase()&&a++&&(e+=" - "),b=b.parent();c+='<option value="'+d.attr("href")+'">'+e+d.text()+"</option>"});$fl(this).addClass("hidden-phone").after('<select id="collapsedMenu'+a+'" class="visible-phone">'+c+"</select>");$fl("#collapsedMenu"+a).bind("change",
-function(){window.location=$fl(this).val()})})});
+$fl(document).ready(function(){$fl(".collapseMenu").each(function(a){var c='<option value="" selected="selected">Menu...</option>';$fl(this).find("a").each(function(){for(var a=$fl(this),e=0,d="",b=a.parent();null!=b[0];)"ul"==b[0].nodeName.toLowerCase()&&e++&&(d+=" - "),b=b.parent();c+='<option value="'+a.attr("href")+'">'+d+a.text()+"</option>"});$fl(this).addClass("hidden-phone").after('<select id="collapsedMenu'+a+'" class="visible-phone">'+c+"</select>");$fl("#collapsedMenu"+a).bind("change",
+function(){window.location=$fl(this).val()})});$fl(".uploadButton input").bind("change",function(a){$fl(this).parent().find("span").html($fl(this).val().split(/(\\|\/)/g).pop())})});
